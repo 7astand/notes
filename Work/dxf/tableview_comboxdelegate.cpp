b39554c512a769a2b59453867b53fea3746d9050
lastand <xu272760313@163.com>
2021-07-05 18:20:11 +0800

    qt model view delegate

---
+++ Work/dxf/tableview_comboxdelegate.cpp
@@ -0,0 +1,21 @@
+tableview
+
+board.getAllLayerStacks();
+layerstack.getALlLayers();
+
+ComboboxColumnTwo
+ComboboxColumnThree
+
+class TableView {
+    LayerStack& getSubclass(row);
+
+}
+
+new ComboboxColumnThree(row) {
+    LayerStack& stack = tableview.getSubClass(row);
+    foreach(Layer* layer, stack.getAllLayers()) {
+        addItem(layer->getName());
+    }
+}
+
+tableView.setText(row, column, "");
